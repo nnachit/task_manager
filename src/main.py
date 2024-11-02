14f4af0de24ea18246f46de151425ecdb6c118ab
nabilou <nabil.nachit@gmail.com>
2024-11-02 19:35:20 +0100

    pep8

--- src/main.py
+++ src/main.py
@@ -2,14 +2,16 @@ import customtkinter as ctk
 from tkinter import messagebox
 
 
-class TodoApp():
+class TodoApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Todo List")
 
         self.tasks = []
 
-        self.title_label = ctk.CTkLabel(self.root, text="Daily Tasks", font=ctk.CTkFont(size=30))
+        self.title_label = ctk.CTkLabel(
+            self.root, text="Daily Tasks", font=ctk.CTkFont(size=30)
+        )
         self.title_label.pack()
 
         self.task_input = ctk.CTkEntry(self.root, width=500)
@@ -18,10 +20,14 @@ class TodoApp():
         self.task_frame = ctk.CTkScrollableFrame(self.root, width=500, height=150)
         self.task_frame.pack(pady=10)
 
-        self.add_task_button = ctk.CTkButton(self.root, width=500, text="Add Task", command=self.add_task)
+        self.add_task_button = ctk.CTkButton(
+            self.root, width=500, text="Add Task", command=self.add_task
+        )
         self.add_task_button.pack(pady=10)
 
-        self.delete_task_button = ctk.CTkButton(self.root, width=500, text="Delete Task", command=self.delete_task)
+        self.delete_task_button = ctk.CTkButton(
+            self.root, width=500, text="Delete Task", command=self.delete_task
+        )
         self.delete_task_button.pack(pady=10)
 
     def add_task(self):
@@ -31,11 +37,17 @@ class TodoApp():
             self.update_task()
             self.task_input.delete(0, ctk.END)
         else:
-            messagebox.showerror("Error", "The task entry is empty")
+            messagebox.showwarning("Warning", "You must enter a task.")
 
     def delete_task(self):
-        # TODO : delete selected task    
-        pass
+        try:
+            selected_task_index = self.task_frame.curselection()[
+                0
+            ]  # Récupère l'index de l'élément sélectionné
+            del self.tasks[selected_task_index]
+            self.update_task()
+        except IndexError:
+            messagebox.showwarning("Warning", "You must select a task to delete.")
 
     def update_task(self):
         for widget in self.task_frame.winfo_children():
@@ -44,6 +56,7 @@ class TodoApp():
         for task in self.tasks:
             task_label = ctk.CTkLabel(self.task_frame, text=task, anchor="w")
             task_label.pack(fill="x", pady=2)
+
 
 if __name__ == "__main__":
     root = ctk.CTk()
